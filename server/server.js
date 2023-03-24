61863df1642bf0c2618c0c8647106cb8c2e7b692
bgrowcott <bgrowcott@ucb.ac.uk>
2023-03-24 11:12:48 +0000

    fixed issue around routing

--- server/server.js
+++ server/server.js
@@ -5,15 +5,18 @@ const { authMiddleware } = require("./utils/auth");
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
 const db = require("./config/connection");
-const routes = require('./api');
+const routes = require("./api");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.static(path.join(__dirname, "../client/build")));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 app.use(express.urlencoded({ extended: true }));
 app.use(authMiddleware);
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "../client/build")));
 app.use(routes);
 app.use(
     session({
@@ -27,14 +30,14 @@ app.use(
             // secure tells express-session to only initialize session cookies when the protocol being used is HTTPS. Having this set to true, and running a server without encryption will result in the cookies not showing up in your developer console.
             secure: false,
             // sameSite tells express-session to only initialize session cookies when the referrer provided by the client matches the domain out server is hosted from.
-            sameSite: 'strict',
+            sameSite: "strict",
 
-            test: 'hello'
-        }
+            test: "hello",
+        },
     })
 );
 
-db.once('open', () => {
+db.once("open", () => {
     app.listen(PORT, () => {
         console.log(`API server running on port ${PORT}!`);
     });
